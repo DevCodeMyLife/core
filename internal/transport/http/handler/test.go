af360483c18b493483e423e9d06ae8186e9cef3c
AndreySHSH <laptev.andrey@icloud.com>
2022-08-20 02:44:34 +0300

    test

--- internal/transport/http/handler/test.go
+++ internal/transport/http/handler/test.go
@@ -1,22 +1,16 @@
 package handler
 
 import (
-	"github.com/ivahaev/go-logger"
 	"github.com/kataras/iris/v12"
 )
 
 func (s *Services) HealthCheck(ctx iris.Context) {
 	ctx.StatusCode(200)
 
-	err := ctx.JSON(iris.Map{
+	_ = ctx.JSON(iris.Map{
 		"status": iris.Map{
 			"message": nil,
 		},
 		"data": nil,
 	})
-	if err != nil {
-		logger.Error(err)
-	}
-
-	return
 }
